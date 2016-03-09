acd293a07413baf6def66793216c1d40a92abf31
Lei Wang <wang.bonn@gmail.com>
2016-03-09 16:12:59 +0100

    feat(Application): function gpio

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var fs = require('fs');
 var _ = require('lodash');
 
-const PINS_FILE = '/sys/kernel/debug/pinctrl/1c20800.pinctrl/pins';
 
 /**
  *
@@ -19,17 +18,90 @@ function getPins(filename) {
     if (!filename) return list;
 
 
-    const lines = fs.readFileSync(filename).toString().split("\n").filter(function (line) {
-        return line != '';
+    var lines = fs.readFileSync(filename).toString().split("\n").filter(function (line) {
+        return _.isString(line);
     });
-    const pinLines = lines.slice(1, lines.length);
+    var pinLines = lines.slice(1, lines.length);
 
     for (var i = 0; i < pinLines.length; i++) {
         var words = _.words(pinLines[i]);
-        list[i] = {pin: parseInt(words[1]), gpio: words[2] + words[3]};
+        list[i] = {pin: _.parseInt(words[1]), gpio: words[2] + words[3]};
     }
 
     return list;
 }
 
-module.exports.getPins = getPins;
+/**
+ *
+ * @param {String} direction - "in" or "out", default: "out"
+ * @returns {*}
+ */
+function sanitizeDirection(direction) {
+    direction = (direction || "").toLowerCase().trim();
+    if (direction === "in" || direction === "input") {
+        return "in";
+    } else if (direction === "out" || direction === "output" || !direction) {
+        return "out";
+    } else {
+        throw new Error("Direction must be 'input' or 'output'");
+    }
+}
+
+/**
+ *
+ * @param {Array} options
+ *                      - pinsFilename
+ *
+ */
+function gpio(options) {
+
+    this.pinsFilename = '/sys/kernel/debug/pinctrl/1c20800.pinctrl/pins';
+    this.gpioFilename = '/sys/class/gpio';
+    this.options = options;
+
+    if (this.options && this.options.pinsFilename) {
+        this.pinsFilename = this.options.pinsFilename;
+    }
+
+    this.pins = getPins(this.pinsFilename);
+
+    this.activatePinsOutput = function (gpioKeys) {
+        var self = this;
+        _(gpioKeys).forEach(function (key) {
+            self.setDirection(key, 'out');
+        });
+    }
+
+    this.setDirection = function (gpioKey, direction) {
+        var pin = this.getPin(gpioKey);
+        if (_.isNull(pin)) {
+            throw new Error(gpioKey + ' not found');
+        }
+
+        fs.writeFile(this.gpioFilename + "/gpio" + pin + "/direction", sanitizeDirection(direction), function (err) {
+            if (err) throw err;
+        });
+    }
+
+    this.getPin = function (gpioKey) {
+        var found = _.find(this.pins, _.matchesProperty('gpio', gpioKey));
+        if (_.isObject(found)) {
+            return found.pin;
+        }
+        return null;
+    }
+
+    this.write = function (gpioKey, signal) {
+        if (_.isEmpty(signal))
+            signal = '0';
+        var pin = this.getPin(gpioKey);
+        if (_.isNull(pin)) {
+            throw new Error(gpioKey + ' not found');
+        }
+        fs.writeFile(this.gpioFilename + "/gpio" + pin + "/value", signal, 'utf8', function (err) {
+            if (err) throw err;
+        });
+    }
+}
+
+module.exports = gpio;
--- test/unit-test.js
+++ test/unit-test.js
@@ -1,13 +1,44 @@
 'use strict';
 
-var gpio = require('../index');
-var assert = require('should');
+var GPIO = require('../index');
+require('should');
+var sinon = require('sinon');
+var fs = require('fs');
+var _ = require('lodash');
 
-describe('get Pins', function () {
-    it('returns array', function () {
-        var pins = gpio.getPins(__dirname + '/fixture/pins');
-        pins.should.be.instanceof(Array).and.have.lengthOf(175);
-        pins[6].should.have.property('pin', 6);
-        pins[6].should.have.property('gpio', 'PA6');
+
+describe('GPIO', function () {
+
+
+    afterEach(function () {
+        fs.writeFile.restore();
     });
+
+    it('actives pins ', function () {
+        var writer = sinon.spy(fs, 'writeFile');
+        var gpio = new GPIO({pinsFilename: __dirname + '/fixture/pins'});
+        var compiled = _.template('/sys/class/gpio/gpio<%= pin %>/direction');
+        gpio.activatePinsOutput(['PG8', 'PG6', 'PG2', 'PG3']);
+        writer.callCount.should.be.equal(4);
+        writer.args[0][0].should.be.equal(compiled({pin: 200}));
+        writer.args[0][1].should.be.equal('out');
+        writer.args[1][0].should.be.equal(compiled({pin: 198}));
+        writer.args[1][1].should.be.equal('out');
+        writer.args[2][0].should.be.equal(compiled({pin: 194}));
+        writer.args[2][1].should.be.equal('out');
+        writer.args[3][0].should.be.equal(compiled({pin: 195}));
+        writer.args[3][1].should.be.equal('out');
+    });
+
+    it('write pin ', function () {
+        var writer = sinon.spy(fs, 'writeFile');
+        var gpio = new GPIO({pinsFilename: __dirname + '/fixture/pins'});
+        var compiled = _.template('/sys/class/gpio/gpio<%= pin %>/value');
+        gpio.write('PG8', '1');
+        writer.callCount.should.be.equal(1);
+        writer.args[0][0].should.be.equal(compiled({pin: 200}));
+        writer.args[0][1].should.be.equal('1');
+    });
+
+
 });
